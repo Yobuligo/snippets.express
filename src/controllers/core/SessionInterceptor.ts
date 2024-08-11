2ca64bdb93044771afee9d32d4d4b1462234a6cb
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-11 09:06:45 +0200

    Introduce NoSessionError and InvalidSessionError

--- src/controllers/core/SessionInterceptor.ts
+++ src/controllers/core/SessionInterceptor.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { SessionRepo } from "../../repositories/sessionRepo/SessionRepo";
+import { InvalidSessionError } from "../../shared/errors/InvalidSessionError";
+import { NoSessionError } from "../../shared/errors/NoSessionError";
 import { createError } from "../../shared/utils/createError";
 import { ISessionRequest } from "./types/ISessionRequest";
 
@@ -13,13 +15,17 @@ export const SessionInterceptor = (
   return async (req: Request, res: Response, next: NextFunction) => {
     const sessionId = req.query.token?.toString();
     if (!sessionId) {
-      return res.status(401).send(createError("No session found"));
+      return res
+        .status(401)
+        .send(createError("No session found", NoSessionError.name));
     }
 
     const sessionRepo = new SessionRepo();
     const session = await sessionRepo.findById(sessionId);
     if (!session) {
-      return res.status(401).send(createError("Invalid session"));
+      return res
+        .status(401)
+        .send(createError("Invalid session", InvalidSessionError.name));
     }
 
     const sessionRequest = req as ISessionRequest;
