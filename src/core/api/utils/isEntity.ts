4000eb078b6fbb70a2deb087ff753daa1eb291de
Yobuligo <hoffmannpeter82@gmail.com>
2024-08-28 14:46:22 +0200

    Update isEntity

--- src/core/api/utils/isEntity.ts
+++ src/core/api/utils/isEntity.ts
@@ -3,7 +3,9 @@ import { IEntity } from "../types/IEntity";
 /**
  * Returns if the given {@link value} is of type {@link IEntity}.
  */
-export const isEntity = (value: object | undefined): value is IEntity => {
+export const isEntity = (
+  value: object | undefined | null
+): value is IEntity => {
   if (!value) {
     return false;
   }
